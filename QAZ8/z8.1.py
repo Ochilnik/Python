c5f1befe8c2a7533cab0abe46639c4379d5797c1
Andrii Cherniavskyi <ochilnik@gmail.com>
2024-04-09 15:46:20 +0300

    module 4 correct

--- QAZ8/z8.1.py
+++ QAZ8/z8.1.py
@@ -2,7 +2,7 @@ number = int(input("Введіть число "))
 digits = []
 
 # your code goes here
-for i in range(0, number + 1):
+for i in range(number + 1):
     digits.append(i)
 
 print(digits)
--- QAZ8/z8.2.py
+++ QAZ8/z8.2.py
@@ -1,8 +1,12 @@
 number = int(input("Введіть число "))
-a= [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # your code goes here
-if number in a:
+res = False
+for i in a:
+    if number == a[i]:
+        res = True
+if res == True:
     print("Введене число - існує")
 else:
     print("Введене число - не існує")
