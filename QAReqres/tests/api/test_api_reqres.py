6f1e86e9e316671feec7050668eb6cc9190aa764
Andrii Cherniavskyi <ochilnik@gmail.com>
2024-06-03 13:13:44 +0300

    Reqres API Test add repeat

--- QAReqres/tests/api/test_api_reqres.py
+++ QAReqres/tests/api/test_api_reqres.py
@@ -4,15 +4,16 @@ import pytest
 @pytest.mark.api
 def test_list_users(reqres_api):
     r = reqres_api.list_users()
-#    print(r)
+    #print(r)
     assert r['data'] != 0
 
 @pytest.mark.api
 def test_list_user(reqres_api):
-    user_num = 3
-    r = reqres_api.list_user(user_num)
-#    print(r['data']['id'])
-    assert r['data']['id'] == user_num
+    num = reqres_api.list_users()
+    for user_num in range(1, num['total']+1):
+        r = reqres_api.list_user(user_num)
+        #print(r['data']['id'])
+        assert r['data']['id'] == user_num
 
 @pytest.mark.api
 def test_no_user(reqres_api):
@@ -24,15 +25,16 @@ def test_no_user(reqres_api):
 @pytest.mark.api
 def test_list_resources(reqres_api):
     r = reqres_api.list_resources()
-#    print(r)
+    #print(r)
     assert r['data'] != 0
 
 @pytest.mark.api
 def test_list_resource(reqres_api):
-    res_num = 2
-    r = reqres_api.list_resource(res_num)
-#    print(r)
-    assert r['data']['id'] == 2
+    num = reqres_api.list_resources()
+    for res_num in range(1, num['per_page']+1):
+        r = reqres_api.list_resource(res_num)
+        #print(r['data']['id'])
+        assert r['data']['id'] == res_num
 
 @pytest.mark.api
 def test_no_resource(reqres_api):
